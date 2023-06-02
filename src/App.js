95127d5d0247a6b9353b60438d64bf720d46f2d2
Thayna00 <oppakino5@hotmail.com>
2023-06-02 16:31:00 -0300

    contêiner dos botões numéricos e de operação da calculadora.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,24 @@ function App() {
     <div className="calc">
        <div className="resultado"> ..</div>
        
-       
+       <div className="num">
+         <button>limpar</button>
+         <button>/</button>
+         <button>*</button>
+         <button>-</button>
+         <button>7</button> 
+         <button>8</button>
+         <button>9</button>
+         <button>+</button>
+         <button>4</button>
+         <button>5</button>
+         <button>6</button>
+         <button>1</button>
+         <button>2</button>
+         <button>3</button>
+         <button>R</button>
+         <button>0</button>
+      </div>
     </div>
     <div className="historico">Resultado e histórico</div>
   </>

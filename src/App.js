eb96e1af5b6e59f7a2dd89466cb3e394ae9be72a
Thayna00 <oppakino5@hotmail.com>
2023-06-05 15:04:32 -0300

    Adicionando estilizão simples

--- src/App.js
+++ src/App.js
@@ -2,29 +2,30 @@
 
 function App() {
   return  <>
+    <h1>Calculadora</h1>
     <div className="calc">
-       <div className="resultado"> ..</div>
+       <div className="result">10</div>
        
        <div className="num">
-         <button>limpar</button>
-         <button>/</button>
-         <button>*</button>
-         <button>-</button>
-         <button>7</button> 
+         <button id="c">C</button>
+         <button id="dividir">/</button>
+         <button id="mult">*</button>
+         <button id="sub">-</button>
+         <button>7</button>
          <button>8</button>
          <button>9</button>
-         <button>+</button>
+         <button id="somar">+</button>
          <button>4</button>
          <button>5</button>
          <button>6</button>
          <button>1</button>
          <button>2</button>
          <button>3</button>
-         <button>R</button>
+         <button id="r">=</button>
          <button>0</button>
       </div>
     </div>
-    <div className="historico">Resultado e historico</div>
+    <div className="historico">Resultado e historico<hr/></div>
   </>
 }
 
